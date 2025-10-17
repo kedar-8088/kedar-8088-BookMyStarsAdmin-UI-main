748e139d26231b81c5777e3de44a89d92712cacb
kedar-8088 <kedaralingmyaginamani>
2025-10-17 12:17:21 +0530

    url updated

--- src/BaseUrl.js
+++ src/BaseUrl.js
@@ -1,2 +1,2 @@
-// export const BaseUrl = 'https://wsproductspostgre.cloudjiffy.net/kwinlabsadmin';
-export const BaseUrl = 'http://localhost:8082';
+export const BaseUrl = 'https://wsproductspostgre.cloudjiffy.net';
+// export const BaseUrl = 'http://localhost:8082';
