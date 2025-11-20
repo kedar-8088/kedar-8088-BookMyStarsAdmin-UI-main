bcaac90fffefb0081e8f34b9d6b7438ed4b3bbe1
kedar-8088 <kedaralingmyaginamani>
2025-11-20 16:05:10 +0530

    Updated

--- src/views/utilities/dashboard/Banner.jsx
+++ src/views/utilities/dashboard/Banner.jsx
@@ -42,9 +42,6 @@ const AuthImage = ({ filePath }) => {
                 setSrc(imageUrl);
             } catch (error) {
                 console.error('Error fetching image:', error);
-                if (error.response?.status === 404) {
-                    console.warn(`Image not found: ${filePath}`);
-                }
                 setSrc(''); // Optionally set a placeholder image here
             }
         };
@@ -108,6 +105,8 @@ const Banner = () => {
         'Content-Type': 'application/json',
         ...(accessToken ? { Authorization: `Bearer ${accessToken}` } : {})
     };
+
+    const ImageUrl = `${BaseUrl}/file/downloadFile/?filePath=`;
 
     const FetchData = async () => {
         try {
